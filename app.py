ad1a1537deef55ab201a85fa31aded1cba58229a
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-12-07 18:15:11 -0300

    criando nova página conteúdo.

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/conteudo")
+def conteudo():
+    return render_template("conteudo.html")
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT",5000))
     app.run(debug=True, host='0.0.0.0', port=port)
